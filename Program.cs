5242c888b9de670058339a7f28287cfb647ead9a
VILAS DESAI <113314297+200522471@users.noreply.github.com>
2022-10-29 16:10:44 -0400

    C#-PRJ-01

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace C__Practice;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        //string N1 = "Vilas" +" in";
+        //int A1 = 20;
+        //Console.WriteLine("name 1 " + N1);
+        //Console.WriteLine("age "+ A1 + " year old");
+ 
+       // Console.WriteLine("My name is\n" +N1);  //    
+
+     /*   Console.WriteLine(N1.Length); //Lengh mate 
+        Console.WriteLine(N1.ToUpper()); //chartachter  upper case
+        Console.WriteLine(N1.Contains("in")); //string ma 6 ke nathi 
+        Console.WriteLine(N1[0]); //print first letter 
+        Console.WriteLine(N1.IndexOf("in")); //string ma in ne position batvse
+        Console.WriteLine(N1.IndexOf('i'));//position number batvse i ne 
+        Console.WriteLine(N1.Substring(5)); //index 5 thi badhu print karse 
+        Console.WriteLine(N1.Substring(1,3 )); //index 1 thi aagad na 3 number sudhu print karse
+        Console.WriteLine(N1.Substring(N1.IndexOf("il"))); //ila thi badhu print karse
+        Console.ReadLine();*/
+
+        string[] a = new string[4];
+        a[0] = "Fenil";
+        a[1] = "vilas";
+        a[2] = "jeel";
+        
+        Console.WriteLine(a[2]);
+    }
+}
